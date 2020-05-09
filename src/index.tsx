72e3668b6563dfdf0db540f68d68c9ae956eb092
Nathan <nph29@drexel.edu>
2020-05-09 10:23:42 -0400

    Test React page

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import MainComponent from "./mainComponent";
+
+
+ReactDOM.render(
+    <MainComponent />,
+    document.getElementById("react_content")
+);
---
+++ src/mainComponent.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export default function MainComponent() {
+    return <div>
+        <h1>React test</h1>
+        <p>How you doin?</p>
+    </div>;
+}
