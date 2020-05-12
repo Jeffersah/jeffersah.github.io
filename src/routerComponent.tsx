32981f796646882ed3edc572c7dd5bd96f2cf50b
Nathan <nph29@drexel.edu>
2020-05-12 16:03:01 -0400

    Switch to using browswerrouter

--- src/routerComponent.tsx
+++ src/routerComponent.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { HashRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import { HomeComponent, ProjectsComponent } from './views';
 
 export default function RouterComponent() {
-    return <HashRouter basename='/'>
+    return <BrowserRouter basename='/'>
         <Route exact path='/' component={HomeComponent} />
         <Route path='/projects/' component={ProjectsComponent} />
-    </HashRouter>;
+    </BrowserRouter>;
 }
